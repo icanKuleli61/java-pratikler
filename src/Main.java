6e59e714be276cd834f2cfea081e68d28c52621c
ilyasCan <ilyaskuleli61@gmail.com>
2025-08-09 21:05:16 +0300

    Not ortalaması proje eklendi

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int mat,fizik,kimya,turkce,tarih,muzik;
+
+        Scanner input = new Scanner(System.in);
+
+        kullaniciMesaj("Matematik notunu giriniz :  ");
+        mat = input.nextInt();
+        kullaniciMesaj("fizik notunu giriniz :  ");
+        fizik = input.nextInt();
+        kullaniciMesaj("kimya notunu giriniz :  ");
+        kimya = input.nextInt();
+        kullaniciMesaj("Türkce notunu giriniz :  ");
+        turkce = input.nextInt();
+        kullaniciMesaj("Tarih notunu giriniz :  ");
+        tarih = input.nextInt();
+        kullaniciMesaj("Muzik notunu giriniz :  ");
+        muzik = input.nextInt();
+
+        float result = (mat + fizik +  kimya + turkce + tarih + muzik)/6f;
+
+        String basarıDurumu = (result >= 60) ? "Ders Ortalaman :"+result+ "Olduğun'dan sınıfı gectiniz" :
+                "Ders Ortalaman : " +result + "Olduğun'dan sınıf'da kaldınız";
+
+        kullaniciMesaj(basarıDurumu);
+
+    }
+
+    public static void kullaniciMesaj(String mesaj){
+        System.out.print(mesaj);
+    }
+}
